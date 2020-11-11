6193f07abb3c1c81fe33129ffe56b0c115d7d7af
Tracy Jiang <tracyjiangmx@gmail.com>
2020-11-11 15:37:33 -0800

    sameFreq and Objarray problems

--- same-frequency.js
+++ same-frequency.js
@@ -1,3 +1,30 @@
-// add whatever parameters you deem necessary
-function sameFrequency() {
+'use strict';
+
+/*this function takes in two positive integers and returns 
+a boolean indicating whether the frequency of digits of the two integers are the same */
+
+function sameFrequency(integer1, integer2) {
+	let int1String = String(integer1);
+	let int2String = String(integer2);
+
+	if (int1String.length !== int2String.length) return false;
+
+	const int1FrequencyCounter = createFrequencyCounter(int1String); 
+	const int2FrequencyCounter = createFrequencyCounter(int2String);
+
+	for (let key in int1FrequencyCounter) {
+		if (int1FrequencyCounter[key] !== int2FrequencyCounter[key]) {
+			return false;
+		}
+	}
+
+	return true;
+}
+
+function createFrequencyCounter(str) {
+  let frequencyCounter = {};
+  for (let letter of str) {
+    frequencyCounter[letter] = (frequencyCounter[letter] || 0) + 1;
+  }
+  return frequencyCounter;
 }
--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,20 @@
+'use strict';
 // add whatever parameters you deem necessary
-function twoArrayObject() {
+/* this function takes in two arrays and creates an object
+with keys from the first array and values from the second array */
+
+function twoArrayObject(keys, values) {
+	let object = {};
+
+	for (let i = 0; i < keys.length; i++) {
+		let key = keys[i];
+
+		if (values[i] !== undefined) {
+			object[key] = values[i];
+		} else {
+			object[key] = null;
+		}
+	}
+
+	return object;
 }
