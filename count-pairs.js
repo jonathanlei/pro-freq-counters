00b21b401fcce6528322482b717e83d9f69a0fba
Tracy Jiang <tracyjiangmx@gmail.com>
2020-11-11 17:07:33 -0800

    count pairs

--- count-pairs.js
+++ count-pairs.js
@@ -1,19 +1,40 @@
-// add whatever parameters you deem necessary
-/* takes an numbers array and a number, return if there is a pair whose average is equal to the number*/
-function countPairs(numsArray, avg) {
-  let start= 0;
-  let end=numsArray.length-1;
-  while (start<end){
-    let currentAverage=(numsArray[start]+numsArray[end])/2;
-    if (currentAverage===avg){
-      return true; 
-    }
-    if (currentAverage>avg){
-      end--;
-    }
-    if (currentAverage<avg) {
-      start++;
-    }
-  }
-  return false; 
+'use strict';
+/* takes in a number array and a target sum, returns how many pairs add up to the sum */
+
+function countPairs(numsArr, targetSum) {
+	let numFrequencyCounter = createFrequencyCounter(numsArr);
+	let pairCount = 0;
+
+	for (let num of numsArr) {
+		let numNeeded = targetSum - num;
+
+		//if num and numNeeded are the same number, check for freq. value greater than 1
+		if (num === numNeeded && numFrequencyCounter.get(num) > 1) {
+			let numVal = numFrequencyCounter.get(num);
+			pairCount++;
+			numFrequencyCounter.set(num, numVal - 2);
+		} else if (num === numNeeded) {
+			//if num and numNeed are the same number, but only one exist in the array, it's not a pair
+			continue;
+		} else if (numFrequencyCounter.has(numNeeded) && numFrequencyCounter.get(numNeeded) > 0) {
+			//if numNeeded is in the frequency counter, decrease value by 1, add  1 to the pair
+			let numVal = numFrequencyCounter.get(num);
+			let numNeededVal = numFrequencyCounter.get(numNeeded);
+
+			pairCount++;
+			numFrequencyCounter.set(num, numVal - 1);
+			numFrequencyCounter.set(numNeeded, numNeededVal - 1);
+		}
+	}
+
+	return pairCount;
+}
+
+function createFrequencyCounter(numsArr) {
+	let frequencyCounter = new Map();
+	for (let num of numsArr) {
+		let numCount = frequencyCounter.get(num) || 0;
+		frequencyCounter.set(num, numCount + 1);
+	}
+	return frequencyCounter;
 }
--- separate-positive.js
+++ separate-positive.js
@@ -1,32 +1,28 @@
-
-"use strict";
+'use strict';
 // add whatever parameters you deem necessary
 /* takes an array of non-zero numbers and return the original array with positive numbers on the left and 
 negative numbers on the right, orders doesn't matter */
 function separatePositive(numsArray) {
-  let start=0; 
-  let end= numsArray.length-1;
-  while (start<end){
-    // if both are positive, start goes up
-    if (numsArray[start]>0 &&numsArray[end]>0){
-      start++;
-    }
-    // if both negative, end goes down 
-    else if (numsArray[start]<0 &&numsArray[end]<0){
-      end--;
-    }
-    // if start is negative and end is positive,swap, both index change  
-    else if (numsArray[start]<0 && numsArray[end]>0) {
-      // swap
-      [numsArray[start],numsArray[end]]=[numsArray[end],numsArray[start]];
-      start++;
-      end--;
-    }
-    // if start is  positive , end is negative, both index change, don't swap
-    else {
-      start++;
-      end--;
-    }
-  }
-  return numsArray;
+	let start = 0;
+	let end = numsArray.length - 1;
+	while (start < end) {
+		// if both are positive, start goes up
+		if (numsArray[start] > 0 && numsArray[end] > 0) {
+			start++;
+		} else if (numsArray[start] < 0 && numsArray[end] < 0) {
+			// if both negative, end goes down
+			end--;
+		} else if (numsArray[start] < 0 && numsArray[end] > 0) {
+			// if start is negative and end is positive,swap, both index change
+			// swap
+			[ numsArray[start], numsArray[end] ] = [ numsArray[end], numsArray[start] ];
+			start++;
+			end--;
+		} else {
+			// if start is  positive , end is negative, both index change, don't swap
+			start++;
+			end--;
+		}
+	}
+	return numsArray;
 }
